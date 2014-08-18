060b8d76e63cbb222e6fba5a34283599c8012c6b
Manuel Guenther <manuel.guenther@idiap.ch>
2014-08-18 16:57:40 +0200

    Added math config.h file

---
+++ bob/math/include/bob.math/config.h
@@ -0,0 +1,14 @@
+/**
+ * @author Manuel Guenther <manuel.guenther@idiap.ch>
+ * @date Mon Aug 18 16:57:06 CEST 2014
+ *
+ * @brief General directives for all modules in bob.math
+ */
+
+#ifndef BOB_MATH_CONFIG_H
+#define BOB_MATH_CONFIG_H
+
+/* Macros that define versions and important names */
+#define BOB_MATH_API_VERSION 0x0200
+
+#endif /* BOB_MATH_CONFIG_H */
