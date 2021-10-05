f1b8babcebc11568ea5a10f1391c71fe4bdf93c5
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2021-10-05 20:08:14 +0200

    Update test_gsvd.py

--- bob/math/test_gsvd.py
+++ bob/math/test_gsvd.py
@@ -107,7 +107,7 @@ def test_svd_signal():
                        [ 7.04651683e-01,  7.09553332e-01,  2.73037723e-04]])
   
   [U,S,V] = bob.math.svd(A)
-  nose.tools.eq_((abs(U-U_ref) < 1e-8).all(), True)
+  nose.tools.eq_((abs(U-U_ref) < 1e-7).all(), True)
   svd_relations(A)
   
 
